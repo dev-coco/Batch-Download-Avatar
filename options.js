2b2d3c117b9dff45de05e9c85d61d9d187ed8e05
Raz1ner <63853884+dev-coco@users.noreply.github.com>
2022-06-18 15:58:28 -0400

    Add files via upload

--- options.js
+++ options.js
@@ -1,118 +1,115 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("start").addEventListener("click", start);
-});
+/* global chrome, alert, fetch */
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('start').addEventListener('click', start)
+  document.getElementById('copy').addEventListener('click', copyResult)
+  document.getElementById('download').addEventListener('click', download)
+})
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("copy").addEventListener("click", copy_result);
-});
+function copyResult () {
+  chrome.storage.local.get(({
+    saveArray
+  }) => {
+    copy(unique(saveArray).join(',').replace(/,/g, '\n'))
+    alert(`已排除重复，复制结果: ${unique(saveArray).length}个`)
+  })
+}
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("download").addEventListener("click", download);
-});
+function unique (data) {
+  return Array.from(new Set(data))
+}
 
-function copy_result() {
-    chrome.storage.local.get(({
-        save_array
-    }) => {
-        copy(unique(save_array).join(",").replace(/,/g, "\n"));
-        alert(`已排除重复，复制结果: ${unique(save_array).length}个`);
+async function start () {
+  chrome.storage.local.clear()
+  const getSelect = document.getElementById('gender')
+  const index = getSelect.selectedIndex
+  let genderStr, minAgeStr, maxAgeStr
+  if (index === 0) {
+    genderStr = ''
+  } else if (index === 1) {
+    genderStr = 'gender=male'
+  } else if (index === 2) {
+    genderStr = 'gender=female'
+  }
+  const minAge = document.getElementById('minAge').value
+  if (minAge === '') {
+    minAgeStr = ''
+  } else {
+    minAgeStr = '&minimum_age=' + minAge
+  }
+  var maxAge = document.getElementById('maxAge').value;
+  if (maxAge === '') {
+    maxAgeStr = ''
+  } else {
+    maxAgeStr = '&maximum_age=' + maxAge
+  }
+  const avatarCount = document.getElementById('avatarCount').value
+  if (avatarCount === '') {
+    return
+  }
+  const url = []
+  for (let k = 0; k < avatarCount; k++) {
+    const response = await fetch(`https://fakeface.rest/face/json?${genderStr}${minAgeStr}${maxAgeStr}`)
+    const text = await response.text()
+    const imageUrl = JSON.parse(text).image_url
+    url.push(imageUrl)
+    document.getElementById('count').innerText = k + 1
+    chrome.storage.local.set({
+      saveArray: url
     })
+  }
+  const speechInstance = new window.SpeechSynthesisUtterance('头像生成完成')
+  window.speechSynthesis.speak(speechInstance)
 }
 
-function unique(get_data) {
-    return Array.from(new Set(get_data))
+function copy (str) {
+  const input = document.createElement('textarea')
+  document.body.appendChild(input)
+  input.value = str
+  input.focus()
+  input.select()
+  document.execCommand('copy')
+  input.remove()
 }
 
-async function start() {
-    chrome.storage.local.clear();
-    var get_select = document.getElementById("gender");
-    var index = get_select.selectedIndex;
-    if (index == 0) {
-        var gender_str = "";
-    } else if (index == 1) {
-        var gender_str = "gender=male";
-    } else if (index == 2) {
-        var gender_str = "gender=female";
-    }
-    var min_age = document.getElementById("min_age").value;
-    if (min_age == "") {
-        var min_age_str = "";
-    } else {
-        var min_age_str = "&minimum_age=" + min_age;
-    }
-    var max_age = document.getElementById("max_age").value;
-    if (max_age == "") {
-        var max_age_str = "";
-    } else {
-        var max_age_str = "&maximum_age=" + max_age;
-    }
-    var avatar_count = document.getElementById("avatar_count").value;
-    if (avatar_count == "") {
-        return;
-    }
-    var url = [];
-    for (var k = 0; k < avatar_count; k++) {
-        let response = await fetch(`https://fakeface.rest/face/json?${gender_str}${min_age_str}${max_age_str}`)
-        let text = await response.text()
-        var image_url = JSON.parse(text).image_url;
-        url.push(image_url);
-        document.getElementById("count").innerText = k+1;
-        chrome.storage.local.set({
-            save_array: url
-        })
-    }
-    let speechInstance = new window.SpeechSynthesisUtterance("头像生成完成");
-    window.speechSynthesis.speak(speechInstance);
-}
-
-function copy(str) {
-    var input = document.createElement("textarea");
-    document.body.appendChild(input);
-    input.value = str;
-    input.focus();
-    input.select();
-    document.execCommand("Copy");
-    input.remove();
-}
-
-async function download() {
-    var get_select = document.getElementById("gender");
-    var index = get_select.selectedIndex;
-    if (index == 0) {
-        var gender_str = "";
-    } else if (index == 1) {
-        var gender_str = "gender=male";
-    } else if (index == 2) {
-        var gender_str = "gender=female";
-    }
-    var min_age = document.getElementById("min_age").value;
-    if (min_age == "") {
-        var min_age_str = "";
-    } else {
-        var min_age_str = "&minimum_age=" + min_age;
-    }
-    var max_age = document.getElementById("max_age").value;
-    if (max_age == "") {
-        var max_age_str = "";
-    } else {
-        var max_age_str = "&maximum_age=" + max_age;
-    }
-    var avatar_count = document.getElementById("avatar_count").value;
-    if (avatar_count == "") {
-        return;
-    }
-    for (var i = 0; i < avatar_count; i++) {
-        let response = await fetch(`https://fakeface.rest/face/json?${gender_str}${min_age_str}${max_age_str}`)
-        let text = await response.text()
-        var image_url = JSON.parse(text).image_url;
-        const elt = document.createElement("a");
-        elt.setAttribute("href", image_url);
-        elt.setAttribute("download", "file.png");
-        elt.style.display = "none";
-        document.body.appendChild(elt);
-        elt.click();
-        document.body.removeChild(elt);
-    }
-    let speechInstance = new window.SpeechSynthesisUtterance("头像下载完成");
-    window.speechSynthesis.speak(speechInstance);
+async function download () {
+  const getSelect = document.getElementById('gender')
+  const index = getSelect.selectedIndex
+  let genderStr, minAgeStr, maxAgeStr
+  if (index === 0) {
+    genderStr = ''
+  } else if (index === 1) {
+    genderStr = 'gender=male'
+  } else if (index === 2) {
+    genderStr = 'gender=female'
+  }
+  const minAge = document.getElementById('minAge').value
+  if (minAge === '') {
+    minAgeStr = ''
+  } else {
+    minAgeStr = '&minimum_age=' + minAge
+  }
+  const maxAge = document.getElementById('maxAge').value
+  if (maxAge === '') {
+    maxAgeStr = ''
+  } else {
+    maxAgeStr = '&maximum_age=' + maxAge
+  }
+  const avatarCount = document.getElementById('avatarCount').value
+  if (avatarCount === '') {
+    return
+  }
+  for (let i = 0; i < avatarCount; i++) {
+    const response = await fetch(`https://fakeface.rest/face/json?${genderStr}${minAgeStr}${maxAgeStr}`)
+    const text = await response.text()
+    const imageUrl = JSON.parse(text).image_url
+    const elt = document.createElement('a')
+    elt.setAttribute('href', imageUrl)
+    elt.setAttribute('download', 'file.png')
+    elt.style.display = 'none'
+    document.body.appendChild(elt)
+    elt.click()
+    document.body.removeChild(elt)
+  }
+  const speechInstance = new window.SpeechSynthesisUtterance('头像下载完成')
+  window.speechSynthesis.speak(speechInstance)
 }
